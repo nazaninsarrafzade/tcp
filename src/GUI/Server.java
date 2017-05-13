816acdf6d54c8036a28a122f3f706b15bcfb17cf
Alieyeh <Alieyeh@gmail.com>
2017-05-13 13:29:16 +0430

    progressbar

---
+++ src/GUI/Server.java
@@ -0,0 +1,86 @@
+package GUI;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.concurrent.Task;
+
+public class Server extends Task<List<File>>{
+    String name;
+    Server(String name1){name=name1;}
+public void ru () throws Exception{
+        String filename;
+        //System.out.println("Enter File Name: ");
+        Scanner sc=new Scanner(System.in);
+        filename=name;
+        sc.close();
+        while(true)
+        {
+        //create server socket on port 5000
+        ServerSocket ss=new ServerSocket(5001);
+        System.out.println ("Waiting for request");
+        Socket s=ss.accept();
+        System.out.println ("Connected With "+s.getInetAddress().toString());
+        DataInputStream din=new DataInputStream(s.getInputStream());
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+        try{
+        String str="";
+
+        str=din.readUTF();
+        System.out.println("SendGet....Ok");
+
+        if(!str.equals("stop")){
+
+        System.out.println("Sending File: "+filename);
+        dout.writeUTF(filename);
+        dout.flush();
+
+        File f=new File(filename);
+        FileInputStream fin=new FileInputStream(f);
+        long sz=(int) f.length();
+
+        byte b[]=new byte [1024];
+
+        int read;
+
+        dout.writeUTF(Long.toString(sz));
+        dout.flush();
+
+        System.out.println ("Size: "+sz);
+        System.out.println ("Buf size: "+ss.getReceiveBufferSize());
+        int m=0;
+        while((read = fin.read(b)) != -1){
+        dout.write(b, 0, read);
+        dout.flush();
+        m=m+read;
+        System.out.println(((float)m*100)/(float)sz);
+        this.updateProgress(((float)m*100), (float)sz);
+        }
+        fin.close();
+
+        System.out.println("..ok");
+        dout.flush();
+        }
+        dout.writeUTF("stop");
+        System.out.println("Send Complete");
+        dout.flush();
+        }
+        catch(Exception e)
+        {
+        e.printStackTrace();
+        System.out.println("An error occured");
+        }
+        din.close();
+        s.close();
+        ss.close();
+        }
+        }
+protected List<File> call() throws Exception{ List<File> copied = new ArrayList<File>();ru(); return copied;}
+        }
---
+++ src/GUI/progress.java
@@ -0,0 +1,97 @@
+package GUI;
+
+import java.io.File;
+import java.util.List;
+import javafx.scene.control.TextField;
+import javafx.application.Application;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class progress extends Application {
+
+    private Server send;
+
+    @Override
+    public void start(Stage primaryStage) {
+        TextField filenameInput = new TextField("o.txt");
+        final Label label = new Label("send files:");
+        final ProgressBar progressBar = new ProgressBar(0);
+        final ProgressIndicator progressIndicator = new ProgressIndicator(0);
+
+        final Button startButton = new Button("Start");
+        final Button cancelButton = new Button("Cancel");
+
+
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                startButton.setDisable(true);
+                progressBar.setProgress(0);
+                progressIndicator.setProgress(0);
+                cancelButton.setDisable(false);
+
+
+                send = new Server(filenameInput.getText());
+
+
+                progressBar.progressProperty().unbind();
+
+
+                progressBar.progressProperty().bind(send.progressProperty());
+
+
+                progressIndicator.progressProperty().unbind();
+
+
+                progressIndicator.progressProperty().bind(send.progressProperty());
+
+
+                new Thread(send).start();
+
+            }
+        });
+
+        // Cancel
+        cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                startButton.setDisable(false);
+                cancelButton.setDisable(true);
+                send.cancel(true);
+                progressBar.progressProperty().unbind();
+                progressIndicator.progressProperty().unbind();
+                //statusLabel.textProperty().unbind();
+                //
+                progressBar.setProgress(0);
+                progressIndicator.setProgress(0);
+            }
+        });
+
+        FlowPane root = new FlowPane();
+        root.setPadding(new Insets(10));
+        root.setHgap(10);
+
+        root.getChildren().addAll(label, progressBar, progressIndicator, //
+                 startButton, cancelButton);
+
+        Scene scene = new Scene(root, 500, 120, Color.WHITE);
+        primaryStage.setTitle("ProgressBar & ProgressIndicator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+}
